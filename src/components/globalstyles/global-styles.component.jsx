a62b5877daadf1de1443d1b8d65288ed684ae581
Pham Quoc Vuong <Vuongnguyen30702@gmail.com>
2022-06-13 23:24:17 +0700

    Footer-Component

---
+++ src/components/globalstyles/global-styles.component.jsx
@@ -0,0 +1,7 @@
+import './global-styles.css';
+import './grid.css';
+function GlobalStyles({ children }) {
+    return children;
+}
+
+export default GlobalStyles;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom/client';
+import App from '~/App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+import GlobalStyles from '~/components/globalstyles/global-styles.component';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
+    </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function

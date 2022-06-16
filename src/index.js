f32d49e7a4b814ecc16f1d4c7c424c09996b8136
mihu2k <hungt2766@gmail.com>
2022-06-17 02:21:01 +0700

    View Product page

--- src/index.js
+++ src/index.js
@@ -10,14 +10,14 @@ import theme from './common/theme';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <GlobalStyles>
-                <App />
-            </GlobalStyles>
-        </ThemeProvider>
-    </React.StrictMode>,
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyles>
+        <App />
+      </GlobalStyles>
+    </ThemeProvider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
